77f749c1b0379993dc1ac1eb815f6ebb2165f77b
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-10-28 21:07:33 -0700

    creates custom hook

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -5,5 +5,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
+        key: "5723b12a079a4c4f9d252443ed614720"
     }
 })
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,28 +1,8 @@
-import apiClient from "@/Services/api-client";
+import useGames from "@/hooks/useGames";
 import { Text } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface Response {
-  count: number;
-  results: Game[];
-}
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<Response>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((error) => setError(error.message));
-  }, []);
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
