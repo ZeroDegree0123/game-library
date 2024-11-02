b4052d81b13054bdf3e99ceceb6ad83a26e399ce
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-11-02 14:57:49 -0700

    creates reusable fetching hook

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,42 +1,10 @@
-import { useState, useEffect } from "react";
-import { CanceledError } from "axios";
-import apiClient from "@/Services/api-client";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number;
     name: string;
 }
 
-interface Response {
-    count: number;
-    results: Genre[];
-}
+const useGenre = () => useData<Genre>('/genres');
 
-const useGenre = () => {
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<Response>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      }) 
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message)
-        setLoading(false)
-    });
-
-      return () => controller.abort();
-  }, []);
-
-  return {genres, error, isLoading};
-}
-
-export default useGenre
+export default useGenre;
